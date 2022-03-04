77a2e0ea491b069238d93b4da5dd36af0ab9470f
ming.hsu <ming.hsu@linecorp.com>
2022-03-05 01:04:51 +0800

    add cors middleware

--- pkg/upload/init.go
+++ pkg/upload/init.go
@@ -1,9 +1,12 @@
 package upload
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/minghsu0107/go-random-chat/pkg/common"
+)
 
 func InitializeRouter() *Router {
 	svr := gin.Default()
-	svr.Use(CORSMiddleware())
+	svr.Use(common.CORSMiddleware())
 	return NewRouter(svr)
 }
